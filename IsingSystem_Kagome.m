62ab9c636c41f7f43359f49aba5536bc05d5f492
San-sin-sun <160474664+San-sin-sun@users.noreply.github.com>
2024-04-25 22:19:38 +0800

    Update IsingSystem_Kagome.m

--- IsingSystem_Kagome.m
+++ IsingSystem_Kagome.m
@@ -8,24 +8,15 @@ classdef IsingSystem_Kagome
     end
     methods
         function obj = IsingSystem_Kagome(spinstate)
-            sideLength = sqrt(length(spinstate)/3);
-            obj.spinstate = reshape(spinstate, sideLength, 3*sideLength) * 2 - 1;
+            L = sqrt(length(spinstate)/3);
+            obj.spinstate = reshape(spinstate, L, 3*L) * 2 - 1;
         end
         function neigh = getneigh(obj, i, j)
             L = size(obj.spinstate, 1);
-            if mod(i,2) == 0
-        % 对于偶数行的顶点
-                neigh(1) = obj.spinstate(mod(i-2-1, L)+1, mod(j-1-1, L)+1);
-                neigh(2) = obj.spinstate(mod(i-1, L)+1, mod(j-1, L)+1);
-                neigh(3) = obj.spinstate(i, mod(j-1, L)+1);
-                neigh(4) = obj.spinstate(mod(i-2-1, L)+1, j);
-            else
-        % 对于奇数行的顶点
-                neigh(1) = obj.spinstate(mod(i-1, L)+1, j);
-                neigh(2) = obj.spinstate(i, mod(j-1-1, L)+1);
-                neigh(3) = obj.spinstate(mod(i+1-1, L)+1, mod(j-1, L)+1);
-                neigh(4) = obj.spinstate(mod(i-1, L)+1, mod(j+1-1, L)+1);
-            end
+            neigh(1) = obj.spinstate(i, j-1);
+            neigh(2) = obj.spinstate(i, j+1);
+            neigh(3) = obj.spinstate(mod(i-2,L)+1, j-1);
+            neigh(4) = obj.spinstate(mod(i-2,L)+1, j+1);
         end
         function h = geth(obj, i, j)
             % 计算给定格点在交换相互作用下的局部磁场
@@ -36,11 +27,19 @@ classdef IsingSystem_Kagome
         function H = get.H(obj)
             % 计算系统的总能量
             H = 0;
+            L=size(obj.spinstate, 1);
             for i = 1:size(obj.spinstate, 1)
-                for j = 1:size(obj.spinstate, 2)
-                    H = H + geth(obj, i, j);
+                for j = 1:size(obj.spinstate, 1)
+                    H = H + geth(obj, i, 3*(j-1)+2);
                 end
             end
+            J=1;
+            for i=1:size(obj.spinstate, 1)
+                for j=1:size(obj.spinstate, 1)
+                    H = H - J*obj.spinstate(i,3*(j-1)+1)*(obj.spinstate(i,mod(3*(j-1)-1, 3*L)+1)+obj.spinstate(i,3*(j-1)+1+2));
+                end
+            end
+
         end
         function M_sq = get.M_sq(obj)
             % 计算系统的总磁化平方
