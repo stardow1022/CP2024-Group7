8cf836f3149cd56a575a405de65e855423ea8d0e
San-sin-sun <160474664+San-sin-sun@users.noreply.github.com>
2024-05-06 14:16:36 +0800

    Update Metro_spin_flip_Cubic.m

--- MC_Cubic/Metro_spin_flip_Cubic.m
+++ MC_Cubic/Metro_spin_flip_Cubic.m
@@ -3,5 +3,5 @@ function spin = Metro_spin_flip_Cubic(spin)
     i=randi(size(spinstate,1));
     j=randi(size(spinstate,2));
     spinstate(i,j)=-spinstate(i,j);
-    spin=IsingSystem_Cubic((reshape(spinstate,[1,size(spinstate,1)*size(spinstate,2)])+1)./2);
+    spin=IsingSystem_Cubic((reshape(spinstate,[1,size(spinstate,1)*size(spinstate,2)])+1)./2,L);
 end
