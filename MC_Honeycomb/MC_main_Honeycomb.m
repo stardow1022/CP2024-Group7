d9e0d2fee9337fd7ccaf22d73201620b3ff0d099
San-sin-sun <160474664+San-sin-sun@users.noreply.github.com>
2024-05-06 14:13:06 +0800

    Update MC_main_Honeycomb.m

--- MC_Honeycomb/MC_main_Honeycomb.m
+++ MC_Honeycomb/MC_main_Honeycomb.m
@@ -3,7 +3,15 @@ L=3;
 spin0=Initialising_Honeycomb(L);
 for i=1:100000
     spin=Sweep_Honeycomb(spin0,T);
+    E(i)=spin.H/2*L^2;
+    M(i)=sum(spin.spinstate,"all")/2*L^2;
+    M2(i)=spin.M_sq/2*L^2;
 end
-E=spin.H;
-M=sum(spin.spinstate,"all");
-M2=spin.M_sq;
+figure(1)
+plot(E)
+ylabel('E')
+title('Honeycomb')
+figure(2)
+plot(M)
+ylabel('M')
+title('Honeycomb')
